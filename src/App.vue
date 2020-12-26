2143a366bff3e22ef8127ffdfef8286b5959d62d
Saumen95 <saumenroy323@gmail.com>
2020-12-26 12:58:44 +0600

    in progress

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    @{{user.username}} - {{fullName}}
+    <strong>Followers:</strong>{{followers}}
+    <button @click="followUser">Follow</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      followers: 0,
+      user:{
+        id:1,
+        username:'Minix',
+        firstName:'Saumen',
+        lastName:'Roy',
+        email:'saumenroy323@gmail.com',
+        isAdmin:true
+      }
+    }
+  },
+
+  watch:{
+    followers(newFollowersCount, oldFollowerCount){
+      if (oldFollowerCount < newFollowersCount) {
+        console.log(`${this.user.username} has gained a follower`);
+      }
+
+    }
+  },
+  computed:{
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods:{
+    followUser(){
+      this.followers++;
+    },
+    mounted() {
+      this.followUser();
+    },
   }
+
 }
 </script>
 
@@ -23,6 +57,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
